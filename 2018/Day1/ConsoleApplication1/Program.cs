b0d59d823ae231a00ae756879ce716dcded38532
Linnau <adventOfCode@linnau.dk>
2019-12-01 21:26:00 +0100

    Added folder for 2018 challenge

---
+++ 2018/Day1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(
+                "D:\\Code\\AdventOfCode_2018\\AdventOfCode_Day1\\adventOfCode_Day1_input.txt");
+
+            var result = 0;
+            HashSet<int> duplicateFreqDetectionDict = new HashSet<int>();
+            var duplicateFound = false;
+            
+            while (!duplicateFound)
+                foreach (var line in lines)
+                {
+                    int number;
+                    if (int.TryParse(line, out number))
+                        result += number;
+
+                    if (duplicateFreqDetectionDict.Contains(result))
+                    {
+                        duplicateFound = true;
+                        break;
+                    }
+                        
+                    duplicateFreqDetectionDict.Add(result);
+                }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
